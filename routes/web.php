3fe7d4e2de15ed6590b6f4947e11f9fa835d32bf
Artiik-fr <mathieu.artiikamcgaming@gmail.com>
2017-04-30 01:52:23 +0200

    Add a basic Blog system.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'HomeController@index');
 Auth::routes();
 Route::get('/register/confirm/{token}', 'Auth\RegisterController@confirm')->name('register.confirm');
 
+Route::get('/blog', 'Blog\PostController@index')->name('blog');
+Route::get('/blog/{slug}', 'Blog\PostController@show')->name('posts.show');
+Route::get('/blog/category/{slug}', 'Blog\PostController@category')->name('posts.category');
+Route::get('/blog/user/{id}', 'Blog\PostController@user')->name('posts.user')->where('id', '[0-9]+');
